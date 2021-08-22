59531c6ee85793a1916cf52751713e2e21f93ea2
Everezze <83906211+Everezze@users.noreply.github.com>
2021-08-22 22:33:17 +0200

    dynamic hamburger menu (on click) added, readme left

--- navbar.js
+++ navbar.js
@@ -1,5 +1,8 @@
 const hamburger= document.getElementById('hamburger');
+const menu= document.getElementById('mobileMenu');
 
 hamburger.addEventListener('click', function(){
-
+    console.log('clicked');
+    console.log(menu);
+    menu.classList.toggle('active');
 });
